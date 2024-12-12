16408ce3f0463ab08ca1242cdff79364f8d727fa
gina923 <jugene990923@gmail.com>
2024-12-12 16:10:42 +0900

    Deploy to ECS

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;  // 5000번 포트를 기본값으로 설정
+
 
 app.use(cors());
 
@@ -11,9 +12,10 @@ const users = [
   { id: 3, username: 'Bruno', email: 'bruno@apple.com' },
 ];
 
-app.get('/users', (req, res) => {
-  res.json(users); // 사용자 데이터를 반환
-});
+app.get('/', (req, res) => {
+    res.status(200).send('OK');
+  });
+  
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
